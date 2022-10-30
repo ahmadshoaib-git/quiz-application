1ff9611d73959bb5388041a38e877dc0a62394a9
Ahmad Shoaib <ahmad.shoaib88@gmail.com>
2022-10-30 22:02:21 +0500

    chore: morgan logger middleware and winston universal logging library added

--- src/bootstrapper.ts
+++ src/bootstrapper.ts
@@ -1,17 +1,22 @@
 import path from 'path';
 import express, { Application } from 'express';
+import morgan from 'morgan';
+import logger from './lib/logger';
 
 type PortType = {
   port: string | number;
 };
 class ExpressApplication {
   private app: Application;
+
   constructor(private port: PortType, private middlewares: any[], private controllers: any[]) {
     this.app = express();
     this.port = port;
 
     // __init__
     this.setupMiddlewares(middlewares);
+    this.configureAssets();
+    this.setupLogger();
   }
   private setupMiddlewares(middlewares: any[]) {
     middlewares.forEach((middleware) => {
@@ -20,6 +25,16 @@ class ExpressApplication {
   }
   private configureAssets() {
     this.app.use(express.static(path.join(__dirname, '../public')));
+  }
+  private setupLogger() {
+    if (process.env.NODE_ENV === 'development') {
+      this.app.use(morgan('dev'));
+    }
+  }
+  public start() {
+    return this.app.listen(this.port, () => {
+      logger.info(`Application is running on ${this.port}`);
+    });
   }
 }
 
